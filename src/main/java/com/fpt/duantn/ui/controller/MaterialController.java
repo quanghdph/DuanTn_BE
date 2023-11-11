fac52641508a4341795bb480b30ed33d119835b4
mmanhh <hhmanh1807@gmail.com>
2023-11-11 17:58:25 +0700

    fix

--- src/main/java/com/fpt/duantn/ui/controller/MaterialController.java
+++ src/main/java/com/fpt/duantn/ui/controller/MaterialController.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/materials")
+@RequestMapping("/material")
 public class MaterialController {
     
     @Autowired
--- src/main/java/com/fpt/duantn/ui/model/response/PaginationRest.java
+++ src/main/java/com/fpt/duantn/ui/model/response/PaginationRest.java
@@ -1,11 +1,13 @@
 package com.fpt.duantn.ui.model.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.Collections;
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PaginationRest {
     private Long total;
     private List<ProductRest> list;
