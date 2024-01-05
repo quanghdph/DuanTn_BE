342dd656b13c08049c02d3ed37984b910e36a634
quanghdph <quanghdph08157@fpt.edu.vn>
2024-01-05 23:48:45 +0700

    update customer

--- src/main/java/com/fpt/duantn/io/repository/CustomerRepository.java
+++ src/main/java/com/fpt/duantn/io/repository/CustomerRepository.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
     @Query(value = "SELECT c.id, c.first_name, c.last_name, c.email, c.gender, c.dateof_birth, c.phone_number, c. customer_code, " +
-            "b.id AS bill_id, b.total_amount, " +
             "e.id AS employee_id, e.employee_code " +
             "FROM customers c " +
             "JOIN bill b ON c.id = b.customer_id " +
@@ -37,13 +36,13 @@ public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> 
             "cust.first_name,cust.last_name,cust.email,cust.email_verification_token,cust.create_date,cust.update_date, " +
             "cust.encrypted_password,cust.gender,cust.dateof_birth,cust.phone_number,cust.customer_code " +
             "FROM customers cust " +
-            "where 1=1 and (:filter is null or :filter = '' or (cust.last_name like %:filter% or cust.create_date like %:filter% or cust.customer_code like %:filter%))",
+            "where 1=1 and (:filter is null or :filter = '' or (cust.last_name like %:filter%  or cust.customer_code like %:filter%))",
             nativeQuery = true)
     Page<CustomerEntity> filter(@Param("filter") String filter, Pageable pageable);
 
     @Query(value = "SELECT count(1) " +
             "FROM customers cust " +
-            "where 1=1 and (:filter is null or :filter = '' or (cust.last_name like %:filter% or cust.create_date like %:filter% or cust.customer_code like %:filter%))",
+            "where 1=1 and (:filter is null or :filter = '' or (cust.last_name like %:filter%  or cust.customer_code like %:filter%))",
             nativeQuery = true)
     Long count(@Param("filter") String filter);
 
